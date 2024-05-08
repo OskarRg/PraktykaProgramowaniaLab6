f4799876b57e9966ab3884320a8562e2308ab53c
OskarRg <oskar_regner005@wp.pl>
2024-05-08 14:27:15 +0200

    Modified utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,18 @@
-# przykladowe funkcje utils.py
 def add(a: int, b: int) -> int:
+    """Addiction."""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Substraction"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Multiplication"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Division"""
     return a / b
